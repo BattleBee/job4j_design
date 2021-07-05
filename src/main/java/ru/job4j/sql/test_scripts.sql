36876d4773dd8dfc789ebb090ae197338631a854
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-07-05 22:34:21 +0300

    0. Что такое база данных. [#6860]

--- src/main/java/ru/job4j/sql/test_scripts.sql
+++ src/main/java/ru/job4j/sql/test_scripts.sql
@@ -1,4 +1,4 @@
-
+-- Создать таблицу с заданными полями (наименование, тип, размер, запрет нулевых значений)
 create table persons (
 	id serial primary key,
 	first_name varchar (50) not null,
@@ -6,13 +6,19 @@ create table persons (
 	age int not null,
 	address text not null
 )
-
+-- Добавить значение в указанные поля
 insert into persons(first_name, second_name, age, address) values('Jon', 'Snow', 17, 'Winterfell');
 
+-- Изменить значение поля
 update persons set second_name = 'Targaryen', age = 23, address= 'the Wall';
 
+-- добавить колонку в таблицу
+alter table persons add column age int not null;
+
+-- Показать выборку значений таблицы
 select * from persons;
 
+-- Удалить все значения из таблиц
 delete from persons;
 
 
