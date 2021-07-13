5952b3e1f6aa8e5b6b94e4b7cc56453a35c89191
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-07-13 12:58:47 +0300

    Мелкие правки

--- src/main/java/ru/job4j/generics/wildcard/Predator.java
+++ src/main/java/ru/job4j/generics/wildcard/Predator.java
@@ -1,7 +1,5 @@
 package ru.job4j.generics.wildcard;
 
-import ru.job4j.generics.wildcard.Animal;
-
 public class Predator extends Animal {
     @Override
     public String toString() {
--- src/main/java/ru/job4j/generics/wildcard/Tiger.java
+++ src/main/java/ru/job4j/generics/wildcard/Tiger.java
@@ -1,7 +1,5 @@
 package ru.job4j.generics.wildcard;
 
-import ru.job4j.generics.wildcard.Predator;
-
 public class Tiger extends Predator {
     @Override
     public String toString() {
