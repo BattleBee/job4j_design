999ed3cf46ece0ee320161d1af03f9edcfe57f07
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-07-15 20:13:11 +0300

    5.2.1. Реализовать SimpleArray<T> [#156 #177032]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -10,18 +10,25 @@ import java.util.Objects;
  * @param <T> обобщенный тип элементов массива
  */
 public class SimpleArray<T> implements Iterable<T> {
+    /**
+     * массив с обобщенным типом элементов.
+     */
     private T[] arr;
-    int size = 0;
+    /**
+     * указыыает индекс последнего элемента массива
+     */
+    int size;
 
     public SimpleArray(int count) {
         this.arr = (T[]) new Object[count];
+        size = 0;
     }
 
-    /**
-     * Добавляет входной параметр в первую свободную ячейку массива;
-     * @param model значение элемента обобщенного типа
-     * @throws IllegalStateException В массиве больше нет свободного места!
-     */
+/**
+ * Добавляет входной параметр в первую свободную ячейку массива;
+ * @param model значение элемента обобщенного типа
+ * @throws IllegalStateException В массиве больше нет свободного места!
+ */
     public void add(T model) {
         if (size >= arr.length) {
             throw new IllegalStateException("Нет свободного места!");
@@ -31,71 +38,71 @@ public class SimpleArray<T> implements Iterable<T> {
         }
     }
 
-    /**
-     * Заменяет указанным элементом (model) элемент, находящийся по индексу index;
-     * С помощью метода <b>Objects.checkIndex(index, size);</b> проверяется что значение index
-     * находится в пределах ранее добавленных в массив элементов.
-      * @param index индекс заменяемого элемента
-     * @param model элемент для замены значения массива по указанному индексу
-     * @throws IndexOutOfBoundsException значение index вне рамок добавленных элементов.
-     */
+/**
+ * Заменяет указанным элементом (model) элемент, находящийся по индексу index;
+ * С помощью метода <b>Objects.checkIndex(index, size);</b> проверяется что значение index
+ * находится в пределах ранее добавленных в массив элементов.
+  * @param index индекс заменяемого элемента
+ * @param model элемент для замены значения массива по указанному индексу
+ * @throws IndexOutOfBoundsException значение index вне рамок добавленных элементов.
+ */
     public void set(int index, T model) {
         int i = Objects.checkIndex(index, size);
         arr[i] = model;
     }
 
-    /**
-     * Возвращает элемент, расположенный по заданному в параметре индексу;
-     * С помощью метода <b>Objects.checkIndex(index, size);</b> проверяется, что значение index
-     * находится в пределах ранее добавленных в массив элементов.
-     * @param index индекс элемента
-     * @return значение элемента массива по <b>index</b>.
-     * Метод <b>Objects.checkIndex(index, size);</b> проверяет, что значение index
-     * находится в пределах ранее добавленных в массив элементов и возвращает значение <b>index</b>.
-     * @throws IndexOutOfBoundsException значение index вне рамок добавленных элементов.
-     */
+/**
+ * Возвращает элемент, расположенный по заданному в параметре индексу;
+ * С помощью метода <b>Objects.checkIndex(index, size);</b> проверяется, что значение index
+ * находится в пределах ранее добавленных в массив элементов.
+ * @param index индекс элемента
+ * @return значение элемента массива по <b>index</b>.
+ * Метод <b>Objects.checkIndex(index, size);</b> проверяет, что значение index
+ * находится в пределах ранее добавленных в массив элементов и возвращает значение <b>index</b>.
+ * @throws IndexOutOfBoundsException значение index вне рамок добавленных элементов.
+ */
     public T get(int index) {
         return arr[Objects.checkIndex(index, size)];
     }
 
-    /**
-     * Удаляет элемент по аданному в параметре индексу индексу, а также сдвигает  все находящиеся
-     * справа элементы на единицу влево, чтобы в середине массива не образовывалось пустых ячеек;
-     * Метод <b>Objects.checkIndex(index, size);</b> проверяет, что значение index
-     * находится в пределах ранее добавленных в массив элементов и возвращает значение <b>index</b>.
-     * @param index индекс удаляемого элемента.
-     * @throws IndexOutOfBoundsException значение index вне рамок добавленных элементов.
-     */
+ /**
+  * Удаляет элемент по аданному в параметре индексу индексу, а также сдвигает  все находящиеся
+  * справа элементы на единицу влево, чтобы в середине массива не образовывалось пустых ячеек;
+  * Метод <b>Objects.checkIndex(index, size);</b> проверяет, что значение index
+  * находится в пределах ранее добавленных в массив элементов и возвращает значение <b>index</b>.
+  * @param index индекс удаляемого элемента.
+  * @throws IndexOutOfBoundsException значение index вне рамок добавленных элементов.
+  */
     public void remove(int index) {
         int i = Objects.checkIndex(index, size);
         System.arraycopy(arr, i + 1, arr, i, arr.length - i - 1);
         size--;
     }
 
-    /**
-     * Итератор для обьектов класса {@link SimpleArray}
-     * @param <T> обобщенный тип элементов массива
-     */
+/**
+ * Итератор для обьектов класса {@link SimpleArray}
+ * @param <T> обобщенный тип элементов массива
+ */
     class SimpleArrayIterator<T> implements Iterator<T> {
-        /**
-         * выполняет функцию указателя итератора
-         */
+/**
+ * выполняет функцию указателя итератора
+ */
         private int point = 0;
 
-        /**
-         * Проверяет наличие следующего элемента в списке.
-         * @return озвращает true если можно получить следующий элемент и false если нет.
-         */
+/**
+ * Проверяет наличие следующего элемента в списке.
+ * @return озвращает true если можно получить следующий элемент и false если нет.
+ */
         @Override
         public boolean hasNext() {
             return point < size;
         }
 
-        /**
-         * Возвращает элемент списка по индексу указателя и перемещает указатель увеличивая его значение на +1.
-         * @throws NoSuchElementException запрошенный элемент не существует.
-         * @return возвращает элемент списка по индексу указателя.
-         */
+/**
+ * Возвращает элемент списка по индексу указателя и перемещает указатель увеличивая его значение на +1.
+ * @throws NoSuchElementException запрошенный элемент не существует.
+ * @return возвращает элемент списка по индексу указателя.
+ */
         @Override
         public T next() {
             if (!hasNext()) {
@@ -105,10 +112,10 @@ public class SimpleArray<T> implements Iterable<T> {
         }
     }
 
-    /**
-     * Определяет итератор для объектов класса (массивов).
-     * @return итератор
-     */
+/**
+ * Определяет итератор для объектов класса (массивов).
+ * @return выбранный итератор
+ */
     @Override
     public Iterator<T> iterator() {
         return new SimpleArrayIterator<T>();
--- src/main/java/ru/job4j/iterator/BackwardArrayIt.java
+++ src/main/java/ru/job4j/iterator/BackwardArrayIt.java
@@ -3,8 +3,17 @@ package ru.job4j.iterator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Итератор, возвращает значения  списка  чисел в обратном порядке.
+ */
 public class BackwardArrayIt implements Iterator<Integer> {
+    /**
+     * входящий список произвольных чисел для итератора
+     */
     private final int[] data;
+    /**
+     * выполняет функцию указателя
+     */
     private int point;
 
     public BackwardArrayIt(int[] data) {
@@ -12,11 +21,22 @@ public class BackwardArrayIt implements Iterator<Integer> {
         point = data.length - 1;
     }
 
+    /**
+     * Метод проверяет можно ли получить следующий элемент списка, соответствующий
+     * заданному условию, также присваивает значение индекса этого элемента переменной указателю.
+     * @return возвращает значение true если можно получить следующий элемент списка, соответствующий
+     * заданному условию, и false  в противном случае.
+     */
     @Override
     public boolean hasNext() {
         return point >= 0;
     }
 
+    /**
+     * Возвращает элемент списка по индексу указателя и перемещает указатель уменьшая его значение на -1.
+     * @throws NoSuchElementException запрошенный элемент не существует.
+     * @return возвращает элемент списка по индексу указателя.
+     */
     @Override
     public Integer next() {
         if (!hasNext()) {
