6292d88de1f97732d42f51cf1c864bcdb05b93ee
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-07-20 13:54:24 +0300

    0. JDBC [#6863]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,24 +1,27 @@
 package ru.job4j.jdbc;
 
 import ru.job4j.io.Config;
-
-import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Подключение к базе данных
+ * Все необходимые данные(драйвер к БД, url, имя пользователя, пароль) берем из конфигурационного файла настроек,
+ * в данном случае из файла <n>app.properties</n>,который читаем с помощью класса Config
+ */
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        Config conf = new Config("app.properties");
+        var conf = new Config("app.properties"); // указываем из какого конфигурационного файла берем данные
         conf.load();
+        Class.forName(conf.value("driver")); // регистрация драйвера в системе
         String url = conf.value("url");
         String login = conf.value("username");
         String password = conf.value("password");
-        try (Connection connection = DriverManager.getConnection(url, login, password)) {
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getUserName());
-            System.out.println(metaData.getURL());
+        try (var connection = DriverManager.getConnection(url, login, password)) { // соединение с БД
+            DatabaseMetaData metaData = connection.getMetaData(); //содетжит информацию о БД и ее внутренней структуре
+            System.out.println(metaData.getUserName()); // получение имя пользователя
+            System.out.println(metaData.getURL()); // получение url
         }
     }
 }
